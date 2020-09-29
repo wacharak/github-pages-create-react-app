fe62701befa4feeb43683d419ce943425a362010
wachara_hafele <wachara@hafele.co.th>
2020-09-29 15:16:37 +0700

    Updates

---
+++ precache-manifest.4a3d5d0c3dc6bc989248734bb0621158.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69fc1dbeb7d578b81f144d8076b2b349",
+    "url": "/github-pages-create-react-app/index.html"
+  },
+  {
+    "revision": "9f2b0c1d0639dda74485",
+    "url": "/github-pages-create-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c3deb36ea35ce28b0578",
+    "url": "/github-pages-create-react-app/static/js/2.efea42fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-pages-create-react-app/static/js/2.efea42fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f2b0c1d0639dda74485",
+    "url": "/github-pages-create-react-app/static/js/main.4d883034.chunk.js"
+  },
+  {
+    "revision": "fb422e73a6346ef25d68",
+    "url": "/github-pages-create-react-app/static/js/runtime-main.edf507e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-pages-create-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
